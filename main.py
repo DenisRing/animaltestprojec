ce2d7edc76e566b06f4c17bfc167b43986431d69
Denis <denisring@protonmail.com>
2023-02-01 08:03:55 +0000

    Cleaning a little.

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
     pdftitle = Path(filepath).stem
+    name = pdftitle.title()
     #pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell( w=50, h=8, txt=pdftitle.title())
+    pdf.cell( w=50, h=8, txt=name)
     #pdf.output(f"PDFs/{pdftitle}.pdf")
 
 pdf.output("output.pdf")
