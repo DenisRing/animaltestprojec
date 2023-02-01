0108ad96883b8e2116db5cfb7c70a58e4f8aeacc
Denis <denisring@protonmail.com>
2023-02-01 07:49:53 +0000

    Minor changes to make everything in one doc, misunderstood project outline

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from pathlib import Path
 
 filepaths = glob.glob("animals/*txt")
 
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
 for filepath in filepaths:
     pdftitle = Path(filepath).stem
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    #pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell( w=50, h=8, txt=pdftitle.title())
-    pdf.output(f"PDFs/{pdftitle}.pdf")
+    #pdf.output(f"PDFs/{pdftitle}.pdf")
+
+pdf.output("output.pdf")
