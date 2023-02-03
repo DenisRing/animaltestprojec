d8e54dc24eed7dcee86f19a9830a8c548203444f
Denis <denisring@protonmail.com>
2023-02-03 16:54:43 +0000

    Adding multicell

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ for filepath in filepaths:
     pdftitle = Path(filepath).stem
     name = pdftitle.title()
     #pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell( w=50, h=8, txt=name)
     #pdf.output(f"PDFs/{pdftitle}.pdf")
+
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("output.pdf")
